1bd4c19c2f4e6fd64fb1d3c3f7f87007ea9a7816
David Probst <david.probst@tum.de>
2021-03-16 18:08:14 +0100

    HTML outline

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,38 @@ function App() {
   return (
     <div className="app">
       <header>
-        Welcome to the header
+        <h1>Aktivität deutscher Bundesministerien auf GovData.de</h1>
       </header>
       <main>
-        Main
+        <section>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Anzahl Datensätze</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Testministerium</th>
+                <td>5</td>
+              </tr>
+              <tr>
+                <th>Testministerium</th>
+                <td>5</td>
+              </tr>
+              <tr>
+                <th>Testministerium</th>
+                <td>5</td>
+              </tr>
+              {/* Data from API will go here*/ }
+            </tbody>
+          </table>
+        </section>
+       
       </main>
       <footer>
-
+        T4G Challenge - David Probst - 2021
       </footer>
     </div>
   );
